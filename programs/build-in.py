10925fc6427fb9fc244d056c2e804d3f171f027a
Sonali Saraswat <35163421+sonalisaraswat@users.noreply.github.com>
2021-03-11 22:36:31 +0530

    Create build-in.py

---
+++ programs/build-in.py
@@ -0,0 +1,10 @@
+import random
+a = random.randint(1, 10)
+print(a)
+
+# To directly use function without writing module name again and again.
+
+from random import *
+a = randint(1, 10)
+print(a)
+
